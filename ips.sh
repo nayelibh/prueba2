6c206d3f11d32e4de74af94a1caf2f67b4746326
nayelibh <sanniodama@gmail.com>
2018-05-16 15:27:11 -0500

    segundo

---
+++ ips.sh
@@ -0,0 +1,52 @@
+o "  MENU    "
+
+
+echo "a. fecha del dia"
+echo "b. IP"
+echo "c. Identificador de Usuario"
+
+read -p 'de la opcion elegida:  ' num
+
+case $num in
+	 a)
+		date
+	;;
+
+	b)
+		echo "1. Para IP inalambrica "
+		echo "2. Para IP cableada"
+		echo "Escriba la opcion seleccionada"
+		read con
+
+
+		if [ $con = 1 ];
+			then
+			 ifconfig wlp16s0 | grep inet | awk {print'$1, $2'} | head -n 1
+	else
+		if [ $con = 2 ];
+	       		then
+			ifconfig enp0s25 | grep inet | awk {print'$1, $2'} | head -n 1
+	else
+			echo "la opcion seleccionada es incorrecta"
+
+		   fi
+	   	   fi
+
+	 ;;
+																			              c)																				   whoami
+																				      ;;
+	 	
+																				      *)
+																			                      echo "la opcion dada es incorrecta"
+																					      ;;
+																			
+																		
+																			
+																			
+																				 esac
+
+																	
+echo "FIN"
+																										                                                              40,3-10     Final
+
+
